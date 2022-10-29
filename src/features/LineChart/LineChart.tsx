09b8315ea8a9f840d2c50d623afe9ed24ecd72e0
Kevin Allain <kevallain@gmail.com>
2022-10-29 18:59:17 +0100

    Adaptation of labels and dataset according to filterMenuSelection working. Code requires cleaning.

--- src/features/LineChart/LineChart.tsx
+++ src/features/LineChart/LineChart.tsx
@@ -30,45 +30,15 @@ const linearInterpolate = (before: number, after: number, atPoint: number) => {
 
 
 const LineChart: FC<LineChartProps> = ((props) => {
-    // const [labels, setLabels] = useState(['January', 'February', 'March','April']); // const labels = props.labels || []; // const labels = (props.patients)? props.patient[0].medTestA: [1,2,3,4,5]
 
     const selectionAttr = useAppSelector(selectSelection);
     const patients = selectionAttr.patients;
     const patientsSelection = selectionAttr.patientsSelection;
 
-    // if (props.patients){ props.patients.forEach( a=> a.medTests.forEach( l => l.listRecords = l.listRecords.sort( (u,v) => new Date(u.record).getTime() - new Date(v.record).getTime()  ) )); } else { props.patients = new Array<Patient>(); }
-
-    // patients.forEach( a=> a.medTests.forEach( l => l.listRecords = l.listRecords.sort( (u,v) => new Date(u.record).getTime() - new Date(v.record).getTime()  ) ));
-
     const dlSliderAttr = useAppSelector(selectDoubleSlider);
-    // console.log("inside LineChart: ", dlSliderAttr);
 
-    // const firstRecD = (props.patients)?[... props.patients[0].medTests.map( s => [...s.listRecords.map(t => t.record)]) ].map(q => q.map(r=>new Date(r)) ):undefined;
-    // const labels = (firstRecD)? (firstRecD[0].sort( (a,b)=>a.getTime()-b.getTime()) ) : ['1','2','3'];
-    // const dispatch = useAppDispatch();
-    // dispatch(sortPatients);
-
-    console.log("========");
     console.log(dlSliderAttr.valueName, dlSliderAttr.minThreshold, dlSliderAttr.maxThreshold)
     const filterMenuAttr = useAppSelector(selectFilterMenu);
-
-    // console.log({ filterMenuAttr });
-    // // applying tyhe filter 
-    // let filteredPatient = Array<Patient>;
-    // const arrValName = ['voluptate', 'aliquip', 'consectetur', 'laboris'];
-    // for (let p of patients) {
-    //     for (let i in p.medTests) {
-    //         let indexArrFilter = arrValName.indexOf(p.medTests[i].testName);
-    //         if (indexArrFilter !== -1) {
-    //             // NOT WORKING
-    //             p.medTests[i].listRecords.map((a, indx) =>
-    //                 (a.score >= filterMenuAttr.minThreshold[indexArrFilter]
-    //                     && a.score <= filterMenuAttr.maxThreshold[indexArrFilter]) ? a :
-    //                     (indx > 0 && indx < p.medTests[i].listRecords.length - 1) ? linearInterpolate(p.medTests[i].listRecords[indx - 1].score, p.medTests[i].listRecords[indx + 1].score, 1) :
-    //                         (indx === 0) ? p.medTests[i].listRecords[indx + 1].score : p.medTests[i].listRecords[indx - 1].score)
-    //         }
-    //     }
-    // }
 
     const final = [];
     for (let patient of patients) {
@@ -77,10 +47,7 @@ const LineChart: FC<LineChartProps> = ((props) => {
             // 0 -> get ALL records in one structure
             let allRecords: string[] = [];
             patient.medTests[0].listRecords.map(b => allRecords.push(b.record));
-            console.log("Pre filtering allRecords: ", { allRecords });
-            console.log("filterMenuAttr: ", { filterMenuAttr });
             // 1 -> what are the listRecords to filter, according to filterMenu
-            let stupid: string[] = [];
             let toFilterAll: string[] = [];
             // for (let i = 0; i < patient.medTests.length; i++) { patient.medTests.map( md => toFilterAll = toFilterAll.concat(  md.listRecords.filter( a => a.score < filterMenuAttr.minThreshold[i] || a.score > filterMenuAttr.maxThreshold[i] ) map(z => z.record) ) ) }
             for (let i in filterMenuAttr.minThreshold) {
@@ -93,35 +60,29 @@ const LineChart: FC<LineChartProps> = ((props) => {
                     }
                 }
             }
-            console.log("toFilterAll: ", { toFilterAll })
-
-            console.log("stupid: ",{ stupid });
             // 2 -> remove them from ALL labels
             const filteredRecords = allRecords.filter(a => !toFilterAll.includes(a))
-            console.log("Post filtering, filteredRecords: ", { filteredRecords })
+
             // 3 -> load the data with filters for data that doesn't match
-
-            let labels = [...patient.medTests[0].listRecords.filter( a => !stupid.includes(a.record)).map(t => t.record)]; console.log({ labels });
+            let labels = [...patient.medTests[0].listRecords.filter( a => !toFilterAll.includes(a.record)).map(t => t.record)]; console.log({ labels });
             labels = filteredRecords.map( a => a );
-            console.log("labels: ",{labels})
 
-            const options = {
-                responsive: true,
-                plugins: { legend: { position: 'top' as const, }, title: { display: true, text: `Patient ${patient.name}`, },},
-            };
 
             for (let i = 0; i < patient.medTests.length; i++) {
                 // let iterData = []; // for (let pm in patient.medTests) { for (let pmd in patient.medTests[pm].listRecords) { iterData.push({ x: patient.medTests[pm].listRecords[pmd].record, y: patient.medTests[pm].listRecords[pmd].score }) } }
-
                 datasets.push({
                     label: `Dataset ${patient.medTests[i].testName}`,
-                    data: [...patient.medTests[i].listRecords.filter(a => a.score >= filterMenuAttr.minThreshold[i] && a.score <= filterMenuAttr.maxThreshold[i]).map(a => a.score)],
-                    // data: iterData,
+                    data:  [...patient.medTests[i].listRecords.filter(a => labels.includes(a.record) ).map(a => a.score)],
+                    // [...patient.medTests[i].listRecords.filter(a => a.score >= filterMenuAttr.minThreshold[i] && a.score <= filterMenuAttr.maxThreshold[i]).map(a => a.score)], // data: iterData,
                     borderColor: `rgb(${(i + 3) * 700 % 255}, ${(i + 1) * 200 % 255}, ${(i + 1) * 400 % 255})`,
                     backgroundColor: `rgba(${(i + 3) * 700 % 255}, ${(i + 1) * 200 % 255}, ${(i + 1) * 400 % 255}, 0.5)`,
                 })
             }
 
+            const options = {
+                responsive: true,
+                plugins: { legend: { position: 'top' as const, }, title: { display: true, text: `Patient ${patient.name}`, },},
+            };
             const data = { labels , datasets: datasets };
             final.push(<li key={patient.name}> <Line options={options} data={data}></Line></li>);
         }
