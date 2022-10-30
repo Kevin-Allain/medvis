e135cef77235f49b15235ff0336aa46bf311eed0
Kevin Allain <kevallain@gmail.com>
2022-10-30 21:28:34 +0000

    Set up made for tests to be possible. We now have to set diverse tests that will secure future development.

--- src/features/LineChart/LineChart.test.tsx
+++ src/features/LineChart/LineChart.test.tsx
@@ -2,6 +2,11 @@ import {describe, expect, test} from '@jest/globals';
 import { sumTest } from "./LineChart";
 // const LineChart = require ('./LineChart');
 
+/**
+ * About the set up: we followed the approach suggested for webpacks to ignore certain files, e.g. css
+ * More can be read here https://jestjs.io/docs/webpack
+ */
+
 test('Ensuring sum works and set up for testing as well', () => {
     expect( sumTest(1,2) ).toBe(3)
 })
